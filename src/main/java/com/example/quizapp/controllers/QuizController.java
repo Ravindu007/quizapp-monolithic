82a94f0a490a166d1ea1967bc2e892578642e4e6
Ravindu007 <ravindu0504@gmail.com>
2024-01-11 13:19:33 +0530

    quiz fetching

--- src/main/java/com/example/quizapp/controllers/QuizController.java
+++ src/main/java/com/example/quizapp/controllers/QuizController.java
@@ -1,16 +1,17 @@
 package com.example.quizapp.controllers;
 
 import com.example.quizapp.dto.ResponseDTO;
+import com.example.quizapp.entity.Question;
+import com.example.quizapp.entity.QuestionWrapper;
 import com.example.quizapp.repo.QuizRepo;
 import com.example.quizapp.service.QuizService;
 import com.example.quizapp.util.VarList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/quiz")
@@ -48,7 +49,16 @@ public class QuizController {
             return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
         }
     }
+
+    //get quiz by id
+    @GetMapping("/get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+        return quizService.getQuizQuestions(id);
+    }
 }
+
+
+
 
 
 
--- src/main/java/com/example/quizapp/service/QuizService.java
+++ src/main/java/com/example/quizapp/service/QuizService.java
@@ -1,15 +1,21 @@
 package com.example.quizapp.service;
 
 import com.example.quizapp.entity.Question;
+import com.example.quizapp.entity.QuestionWrapper;
 import com.example.quizapp.entity.Quiz;
 import com.example.quizapp.repo.QuestionRepo;
 import com.example.quizapp.repo.QuizRepo;
 import com.example.quizapp.util.VarList;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -32,5 +38,24 @@ public class QuizService {
         return VarList.RSP_SUCCESS;
     }
 
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id) {
+        Optional<Quiz> quiz = quizRepo.findById(id);
+        List<Question> questionFromDb = quiz.get().getQuestions();
+
+        List<QuestionWrapper> questionsForUsers = new ArrayList<>();
+        for(Question q : questionFromDb){
+            QuestionWrapper qw = new QuestionWrapper(
+                    q.getId(),
+                    q.getQuestionTitle(),
+                    q.getOption1(),
+                    q.getOption2(),
+                    q.getOption3(),
+                    q.getOption4()
+            );
+            questionsForUsers.add(qw);
+        }
+
+        return new ResponseEntity<>(questionsForUsers, HttpStatus.OK);
+    }
 }
 
